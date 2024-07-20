0611209c313cbb8ba831a0c9508c9a6c57104900
Lucas Healy <lucasbhealy@gmail.com>
2024-07-20 12:44:55 -0700

    user patch instead of navigate

--- lib/liveViewSlax_web/live/chat_room_live.ex
+++ lib/liveViewSlax_web/live/chat_room_live.ex
@@ -47,31 +47,37 @@ defmodule LiveViewSlaxWeb.ChatRoomLive do
 
   defp room_link(assigns) do
     ~H"""
-    <a
+    <.link
       class={[
         "flex items-center h-8 text-sm pl-8 pr-3",
         (@active && "bg-slate-300") || "hover:bg-slate-300"
       ]}
-      href={~p"/rooms/#{@room}"}
+      patch={~p"/rooms/#{@room}"}
     >
       <.icon name="hero-hashtag" class="h-4 w-4" />
       <span class={["ml-2 leading-none", @active && "font-bold"]}>
         <%= @room.name %>
       </span>
-    </a>
+    </.link>
     """
   end
 
-  def mount(params, _session, socket) do
-    rooms = Repo.all(Room)
-
+  def handle_params(params, _session, socket) do
     room =
       case Map.fetch(params, "id") do
-        {:ok, id} -> Repo.get(Room, id)
-        :error -> List.first(rooms)
+        {:ok, id} ->
+          Repo.get!(Room, id)
+        :error ->
+          List.first(socket.assigns.rooms)
       end
 
-    {:ok, assign(socket, hide_topic?: false, rooms: rooms, room: room)}
+      {:noreply, assign(socket, hide_topic?: false, room: room)}
+  end
+
+  def mount(_params, _session, socket) do
+    rooms = Repo.all(Room)
+
+    {:ok, assign(socket, rooms: rooms)}
   end
 
   def handle_event("toggle-topic", _params, socket) do
