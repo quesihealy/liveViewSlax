17981013bdfada49ab0116bcc84ec142525fc1d9
Lucas Healy <lucasbhealy@gmail.com>
2024-07-20 12:44:55 -0700

    user Ecto.Query in chat context

--- lib/liveViewSlax/chat.ex
+++ lib/liveViewSlax/chat.ex
@@ -2,9 +2,10 @@ defmodule LiveViewSlax.Chat do
   alias LiveViewSlax.Chat.Room
   alias LiveViewSlax.Repo
 
+  import Ecto.Query
+
   def get_first_room! do
-    [room | _] = list_rooms()
-    room
+    Repo.one!(from r in Room, limit: 1, order_by: [asc: :name])
   end
 
   def get_room!(id) do
@@ -12,6 +13,6 @@ defmodule LiveViewSlax.Chat do
   end
 
   def list_rooms do
-    Room |> Repo.all()
+    Repo.all(from r in Room, order_by: [asc: :name])
   end
 end
