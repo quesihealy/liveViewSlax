d34886966a4c6721290a24cad3f2836ac5dcec96
Lucas Healy <lucasbhealy@gmail.com>
2024-09-25 21:16:48 -0700

    convert messages to streams

--- lib/liveViewSlax_web/live/chat_room_live.ex
+++ lib/liveViewSlax_web/live/chat_room_live.ex
@@ -85,8 +85,8 @@ defmodule LiveViewSlaxWeb.ChatRoomLive do
         <% end %>
       </ul>
       </div>
-      <div class="flex flex-col flex-grow overflow-auto">
-        <.message :for={message <- @messages} message={message} />
+      <div id="room-messages" class="flex flex-col flex-grow overflow-auto" phx-update="stream">
+        <.message :for={{dom_id, message} <- @streams.messages} dom_id={dom_id} message={message} />
       </div>
       <div class="h-12 bg-white px-4 pb-4">
         <.form
@@ -127,7 +127,7 @@ defmodule LiveViewSlaxWeb.ChatRoomLive do
       case Chat.create_message(room, message_params, current_user) do
         {:ok, message} ->
           socket
-          |> update(:messages, &(&1 ++ [message]))
+          |> stream_insert(:messages, message)
           |> assign_message_form(Chat.change_message(%Message{}))
 
         {:error, changeset} ->
@@ -164,10 +164,10 @@ defmodule LiveViewSlaxWeb.ChatRoomLive do
       socket
       |> assign(
         hide_topic?: false,
-        messages: messages,
         page_title: "#" <> room.name,
         room: room
       )
+      |> stream(:messages, messages, reset: true)
       |> assign_message_form(Chat.change_message(%Message{}))
     }
   end
@@ -196,7 +196,7 @@ defmodule LiveViewSlaxWeb.ChatRoomLive do
 
   defp message(assigns) do
     ~H"""
-    <div class="relative flex px-4 py-3">
+    <div id={@dom_id} class="relative flex px-4 py-3">
       <div class="h-10 w-10 rounded flex-shrink-0 bg-slate-300"></div>
       <div class="ml-2">
         <div class="-mt-1">
